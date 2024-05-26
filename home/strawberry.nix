46c1b64e275bc57cbc16ee59a3705e7f4c146b62
kyle <kylerichards32@gmail.com>
2024-05-25 20:38:51 -0400

    Update packages

--- home/strawberry.nix
+++ home/strawberry.nix
@@ -47,6 +47,9 @@
 
     obs-studio
     prismlauncher
+    kdialog
+    mangohud
+    vlc
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
