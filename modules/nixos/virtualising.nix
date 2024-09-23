ecbfec0a215b5d0981297890d63ed61b7586b913
kylelovestoad <kylelovestoad+github@protonmail.com>
2024-09-23 17:28:26 -0400

    feat: module for virtual machines

---
+++ modules/nixos/virtualising.nix
@@ -0,0 +1,28 @@
+{lib, kylib, pkgs, ...}: (cfg: {
+
+  options = {
+    users = lib.mkOption { default = []; };
+  };
+
+  config = {
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = false;
+        # Necessary for Windows 11 
+        swtpm.enable = true;
+      };
+      # For UEFI booting
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+
+    users.users = kylib.addGroupsToUsers [ "libvirtd" ] cfg.users;
+  };
+})
