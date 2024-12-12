04c037c3a7a41106f348e7c90da85bed1b7feb8e
kylelovestoad <kylelovestoad+github@protonmail.com>
2024-12-12 05:54:35 -0500

    fix: plum should work now

--- home/plum.nix
+++ home/plum.nix
@@ -14,42 +14,17 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      rustToolchain =
-        let
-          rust = prev.rust-bin;
-        in
-        if builtins.pathExists ./rust-toolchain.toml then
-          rust.fromRustupToolchainFile ./rust-toolchain.toml
-        else if builtins.pathExists ./rust-toolchain then
-          rust.fromRustupToolchainFile ./rust-toolchain
-        else
-          rust.stable.latest.default.override {
-            extensions = [ "rust-src" "rustfmt" ];
-          };
-    })
-  ];
-
   home.packages = with pkgs; [
     nix-direnv
     fastfetch
     nerdfonts
     figlet
 
-    rustToolchain
-    openssl
-    pkg-config
     cargo-deny
     cargo-edit
     cargo-watch
-    rust-analyzer
+    cargo
   ];
-
-  home.sessionVariables = {
-    # Required by rust-analyzer
-    RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-  };
 
   fish.enable = lib.mkForce true;
 }
