66cda5cd1bfdeb946ae41a712238a85a92a7ff6b
kylelovestoad <kylelovestoad+github@protonmail.com>
2024-12-14 04:53:09 -0500

    feat: direnv for plum

--- home/plum.nix
+++ home/plum.nix
@@ -17,16 +17,25 @@
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
-    nix-direnv
     fastfetch
     nerdfonts
     figlet
 
+    # Rust stuff since it isn't installed
     cargo-deny
     cargo-edit
     cargo-watch
     cargo
+    rustc
   ];
+
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+  };
 
   fish.enable = lib.mkForce true;
 }
